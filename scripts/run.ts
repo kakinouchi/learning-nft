447bcc35911fd768d0942f691c6e409e85b65cf6
kakinouchi <32264080+kakinouchi@users.noreply.github.com>
2024-08-22 18:52:35 +0900

    Update the test code

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,21 +1,26 @@
 import { ethers } from "hardhat";
-
 const main = async () => {
   const [owner, randomPerson] = await ethers.getSigners();
   const echoContractFactory = await ethers.getContractFactory("EthEcho");
   const echoContract = await echoContractFactory.deploy();
   const ethEcho = await echoContract.waitForDeployment();
 
-  const deployedContractAddress = await ethEcho.getAddress();
-  console.log("Contract deployed to:", deployedContractAddress);
+  const address = await ethEcho.getAddress();
+  console.log("Contract deployed to:", address);
   console.log("Contract deployed by:", owner.address);
 
   await echoContract.getTotalEchoes();
 
+//   let echoTxn = await echoContract.echoMessage();
   let echoTxn = await echoContract.writeEcho();
   await echoTxn.wait();
 
   await echoContract.getTotalEchoes();
+
+ echoTxn = await echoContract.connect(randomPerson).writeEcho();
+
+ await echoTxn.wait();
+ await echoContract.getTotalEchoes();
 };
 
 const runMain = async () => {
